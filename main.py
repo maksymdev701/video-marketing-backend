8ed263caa51d2687ae7a0c23b9ae477b4b83bd95
maksymdev701 <mmeliukh71@gmail.com>
2023-07-06 15:31:19 +0300

    updates for eb

--- main.py
+++ main.py
@@ -19,5 +19,9 @@ app.include_router(user.router, prefix="/api/users", tags=["Users"])
 app.include_router(video.router, prefix="/api/videos", tags=["Videos"])
 app.include_router(stats.router, prefix="/api/stats", tags=["Stats"])
 
+@app.get("/")
+async def health_checker():
+    return {"status": "success"}
+
 if __name__ == '__main__':
     uvicorn.run("main:app", reload=True, host="0.0.0.0", port=9000)
